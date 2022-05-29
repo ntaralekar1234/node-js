04e8a2b9d5ae0d6506c00a71a3339bfc6be24392
ntaralekar1234 <ntaralekar1234@github.com>
2022-05-29 08:53:26 +0530

    weather app

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,5 +1,22 @@
+const weatherForm = document.querySelector('form')
+const input = document.querySelector('input')
 
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-fetch('http://localhost:3000/weather').then((res) => {
-    console.log(res)
+weatherForm.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+    messageOne.textContent = ""
+    messageTwo.textContent = "loading..."
+    const location = input.value;
+    fetch('http://localhost:3000/weather?address='+location).then((res) => {
+        res.json().then((data) => {
+            if(data.error){
+                return messageTwo.textContent = data.error
+            }
+            messageOne.textContent = data.place_name
+            messageTwo.textContent = data.forecast
+        })
+    })
 })
